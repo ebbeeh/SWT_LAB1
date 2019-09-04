cfd80117290c00717148b07966d16c979a7cc066
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2019-09-04 13:35:41 +0200

    adds new test for Add functionality

---
+++ Calculator/Calculator.Test.Unit/CalculatorTests_add.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+
+    [TestFixture]
+    class CalculatorTests_add
+    {
+        private Calculator uut;
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+        [TestCase(2, 2, 4)]
+        [TestCase(4, 4, 8)]
+        [TestCase(1, 1, 2)]
+        [TestCase(10, 10, 20)]
+        public void test_AddPossitiveNumbers(int a, int b, int result)
+        {
+            Assert.That(result, Is.EqualTo(uut.Add(a, b)));
+        }
+    }
+}
